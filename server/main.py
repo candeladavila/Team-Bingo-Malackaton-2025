34ade8677aeb4aa1c39a5034d768b8d8882822fd
candeladavila <candeladavila05@gmail.com>
2025-10-16 17:23:01 +0200

    updated CORS to allow frontend calls

--- server/main.py
+++ server/main.py
@@ -10,7 +10,11 @@ app = FastAPI(title="Team Bingo Malackaton API", version="1.0.0")
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Añadir más orígenes según sea necesario
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://localhost:3000",
+        "https://insight-client-nine.vercel.app"
+    ],  # Añadir más orígenes según sea necesario
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
