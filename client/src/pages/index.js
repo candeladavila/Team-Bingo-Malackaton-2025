15343fe98c3994be09ca011025c1cf62ef7e12e1
joseC2209 <josecdn10@gmail.com>
2025-10-16 20:04:40 +0200

    Accesibilidad Avanzado

--- client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-// Archivo index para facilitar las importaciones de las páginas
-export { default as LandingPage } from './LandingPage'
-export { default as ToolsPage } from './ToolsPage'
-export { default as DataFilteringPage } from './DataFilteringPage'
-export { default as DataVisualizationPage } from './DataVisualizationPage'
-export { default as AboutPage } from './AboutPage'
-export { default as AccessibilityConfigPage } from './AccessibilityConfigPage'
