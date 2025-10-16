0f4a2650a86a68d16b89a68bc456c9552d2bff04
candeladavila <candeladavila05@gmail.com>
2025-10-16 18:48:49 +0200

    prueba

--- server/main.py
+++ server/main.py
@@ -22,7 +22,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://localhost:3000",
-        "https://insight-client-nine.vercel.app"
     ],  # Añadir más orígenes según sea necesario
     allow_credentials=True,
     allow_methods=["*"],
